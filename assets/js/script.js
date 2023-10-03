466d526658a057cb299ad4032bb43681a5753ef3
Nabeel Haider <138874574+nabeelhaider1-0@users.noreply.github.com>
2023-10-03 21:57:11 +0500

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,42 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+
+// Create variables for each button's state
+let isToggled1 = false;
+let isToggled2 = false;
+let isToggled3 = false;
+let isToggled4 = false;
+
+// Add event listeners to each button
+document.getElementById('toggle-button-1').addEventListener('click', function () {
+  toggleImage('toggle-button-1', 'assets/images/plus.svg', 'assets/images/minus.svg', isToggled1);
+  isToggled1 = !isToggled1;
+});
+
+document.getElementById('toggle-button-2').addEventListener('click', function () {
+  toggleImage('toggle-button-2', 'assets/images/plus.svg', 'assets/images/minus.svg', isToggled2);
+  isToggled2 = !isToggled2;
+});
+
+document.getElementById('toggle-button-3').addEventListener('click', function () {
+  toggleImage('toggle-button-3', 'assets/images/plus.svg', 'assets/images/minus.svg', isToggled3);
+  isToggled3 = !isToggled3;
+});
+
+document.getElementById('toggle-button-4').addEventListener('click', function () {
+  toggleImage('toggle-button-4', 'assets/images/plus.svg', 'assets/images/minus.svg', isToggled4);
+  isToggled4 = !isToggled4;
+});
+
+// Function to toggle image source
+function toggleImage(buttonId, originalSrc, newSrc, isToggled) {
+  const button = document.getElementById(buttonId);
+  if (isToggled) {
+    button.src = originalSrc;
+  } else {
+    button.src = newSrc;
+  }
+}
